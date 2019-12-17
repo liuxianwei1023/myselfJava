cac6add4a836e1c3895399d9749bb2a96643d468
Your Name <1769608694@qq.com>
2019-12-17 20:05:24 +0800

    是master

--- src/main/java/com/lxw/GitApplication.java
+++ src/main/java/com/lxw/GitApplication.java
@@ -10,7 +10,8 @@ public class GitApplication {
     public static void main(String[] args) {
         SpringApplication.run(GitApplication.class, args);
         System.out.println("第一次做修改");
-        System.out.println("第二次做修改");
-
+        System.out.println("这个是dev的修改");
+        System.out.println("dev Or master?");
+        System.out.println("这个应该是master");
     }
 }
